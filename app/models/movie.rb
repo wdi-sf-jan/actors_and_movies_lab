a4bb973596ee03c1c7821a707ad94c9fc26cc9a6
Tim Licata <tim@timothylicata.com>
2015-02-22 22:20:12 -0800

    movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,13 @@
 class Movie < ActiveRecord::Base
   has_many :actors_movies, dependent: :destroy
   has_many :actors, through: :actors_movies
+
+  validates :title, :presence => true
+  validates :year, {
+              :presence => true,
+              :numericality => {
+                :greater_than_or_equal_to => 1000,
+                :less_than_or_equal_to => 3000
+              }
+            }
 end
