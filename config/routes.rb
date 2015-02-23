bae36940e5a4f9f24109292030556f1e1608da73
Tim Licata <tim@timothylicata.com>
2015-02-22 18:21:36 -0600

    add add_actor, remove_actor, add_movie, and remove_movie routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :movies
   resources :actors
 
+  post '/movies/:id/actors/new' => 'movies#add_actor', as: :add_actor
+  delete 'movies/:id/actors/:actor_id' => 'movies#remove_actor', as: :remove_actor
+
+  post '/actors/:id/movies/new' => 'actors#add_movie', as: :add_movie
+  delete '/actors/:id/movies/:movie_id' => 'actors#remove_movie', as: :remove_movie
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
